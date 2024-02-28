c1fcd62f2a2e68459e97c5a0b3cbdab22ea3626e
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2024-02-28 15:35:47 +0100

    Script for the CP monitoring enhanced

--- scripts/monitor-cp.sh
+++ scripts/monitor-cp.sh
@@ -4,22 +4,32 @@
 expectedReconcilerLabelValue="reconciler"
 expectedOperatorLabelValue="sap.connectivity.proxy.operator"
 
-# Check if the StatefulSet exists in the specified namespace
+cpExists=false
+operatorExists=false
+crdExists=false
+crExists=false
+cpInstalledWithReconciler=false
+cpInstalledWithOperator=false 
+
 statefulSetExists=$(kubectl get statefulset connectivity-proxy -n kyma-system --ignore-not-found)
+echo "**CHECKING RUNTIME**"
 
 if [[ -z "$statefulSetExists" ]]; then
-    echo "Connectivity Proxy is not installed on this cluster"
-    exit 1
+    echo "Connectivity Proxy StatefulSet NOT FOUND."
 else
-    echo "Connectivity Proxy is installed on this cluster"
+    cpExists=true
+
+    echo "Connectivity Proxy StatefulSet found."
     actualLabelValue=$(kubectl get statefulset connectivity-proxy -n kyma-system -o=jsonpath="{.metadata.labels.reconciler\.kyma-project\.io\/managed-by}" --ignore-not-found)
     if [[ "$actualLabelValue" == "$expectedReconcilerLabelValue" ]]; then
-        echo "This Connectivity Proxy installation is managed by the Reconciler"
+       cpInstalledWithReconciler=true 
+       echo "The Connectivity Proxy StatefulSet is managed by the Reconciler"
     fi
 
     actualLabelValue=$(kubectl get statefulset connectivity-proxy -n kyma-system -o=jsonpath="{.metadata.labels.app\.kubernetes\.io\/managed-by}" --ignore-not-found)
     if [[ "$actualLabelValue" == "$expectedOperatorLabelValue" ]]; then
-        echo "This Connectivity Proxy installation is managed by the Connectivity Proxy Operator"
+       cpInstalledWithOperator=true 
+       echo "The Connectivity Proxy StatefulSet is managed by the Connectivity Proxy Operator"
     fi
     
     stateful_set_status=$(kubectl get statefulset connectivity-proxy -n kyma-system -ojsonpath={.status})
@@ -28,15 +38,37 @@ fi
 
 
 if kubectl get crd connectivityproxies.connectivityproxy.sap.com &> /dev/null; then
-   echo "The Connectivity Proxy CRD is installed on the cluster"
+   crdExists=true
+   echo "The Connectivity Proxy CRD found."
+else
+   echo "The Connectivity Proxy CRD NOT FOUND."
 fi
  
 if kubectl get connectivityproxies.connectivityproxy.sap.com/connectivity-proxy -n kyma-system &> /dev/null; then
+   crExists=true
    operator_state=$(kubectl get connectivityproxies.connectivityproxy.sap.com/connectivity-proxy -n kyma-system -ojsonpath={.status.state})
-   echo "Operator state: $operator_state"
+   echo "Connectivity Proxy CR state: $operator_state"
+else
+   echo "The Connectivity Proxy CR NOT FOUND."
 fi
 
 if kubectl get deployment connectivity-proxy-operator -n kyma-system &> /dev/null; then
-   operator_deployment_state=$(kubectl get deploy connectivity-proxy-operator -n kyma-system -ojsonpath={.status.conditions})
+   operatorExists=true
+   operator_deployment_state=$(kubectl rollout status deploy connectivity-proxy-operator -n kyma-system)
    echo "Operator deployment state: $operator_deployment_state"
+else
+   echo "Connectivity Proxy Operator NOT FOUND."
 fi 
+
+# Check status
+printf "\n**SUMMARY**\n"
+if [[ "$cpExists" = false && "$operatorExists" = false  && "$crdExists" = false && "$crExists" = false ]] ; then
+   echo "Connectivity Proxy is not installed on the runtime. Status is OK"
+elif [[ "$cpExists" = true && "$operatorExists" = false && "$crdExists" = false && "$crExists" = false ]] ; then
+   echo "Connectivity Proxy is installed with the Reconciler"
+elif [[ "$cpExists" = true && "$operatorExists" = true && "$crdExists" = true && "$crExists" = true ]] ; then
+   echo "Connectivity Proxy is installed with the Operator"
+else
+   echo "ERROR! The status of the runtime is inconsistent. " 
+fi
+
